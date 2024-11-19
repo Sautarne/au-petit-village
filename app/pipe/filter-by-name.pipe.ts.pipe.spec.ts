a60c41628b0b853c0895e74e70d8ef94f2ddfd1c
Christophe PEREZ <christophe.perez690@gmail.com>
2024-11-19 16:57:26 +0100

    save

--- app/pipe/filter-by-name.pipe.ts.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterByNamePipeTsPipe } from './filter-by-name.pipe.ts.pipe';
-
-describe('FilterByNamePipeTsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterByNamePipeTsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
